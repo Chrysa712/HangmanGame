76252ecff89c205adeb23af43541ee0cc84af81e
Chrysa712 <chrizeakoy@gmail.com>
2022-03-06 13:56:03 +0200

    Fixed hangman_DICTIONARY-XX.txt path

--- src/hangman/HandleFiles.java
+++ src/hangman/HandleFiles.java
@@ -8,7 +8,8 @@ public class HandleFiles {
     public static void CreateFile (String ID) {
         try {
 
-            File myObj = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab", "hangman_DICTIONARΥ-" + ID +".txt");
+            //File myObj = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab", "hangman_DICTIONARΥ-" + ID +".txt");
+            File myObj = new File("medialab\\hangman_DICTIONARΥ-" + ID + ".txt");
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
             } else {
@@ -24,8 +25,10 @@ public class HandleFiles {
     // NOT WORKING YET
     public static void RemoveBlankLine(String ID){
         try{
-            File myObj = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab","hangman_DICTIONARΥ-" + ID +".txt");
-            File myObj2 = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab","hangman_DICTIONARΥ-help.txt");
+            //File myObj = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab","hangman_DICTIONARΥ-" + ID +".txt");
+            File myObj = new File("medialab\\hangman_DICTIONARΥ-" + ID + ".txt");
+            //File myObj2 = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab","hangman_DICTIONARΥ-help.txt");
+            File myObj2 = new File("medialab\\hangman_DICTIONARΥ-help.txt");
             Scanner myReader = new Scanner(myObj);
             FileWriter myWriter = new FileWriter(myObj2);
             //PrintWriter myWriter = new PrintWriter(myObj2);
@@ -54,7 +57,8 @@ public class HandleFiles {
 
     public static void WriteFile(String ID, String data) {
         try {
-            FileWriter myWriter = new FileWriter("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab\\hangman_DICTIONARΥ-" + ID +".txt");
+            //FileWriter myWriter = new FileWriter("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab\\hangman_DICTIONARΥ-" + ID +".txt");
+            FileWriter myWriter = new FileWriter("medialab\\hangman_DICTIONARΥ-" + ID + ".txt");
             //myWriter.write("Files in Java might be tricky, but it is fun enough!");
             //myWriter.close();
             data = data.replaceAll("\\s+",System.getProperty("line.separator"));
@@ -72,7 +76,8 @@ public class HandleFiles {
         String[] res = new String[0];
         try {
             StringBuilder res2 = new StringBuilder();
-            File myObj = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab","hangman_DICTIONARΥ-" + ID +".txt");
+            //File myObj = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab","hangman_DICTIONARΥ-" + ID +".txt");
+            File myObj = new File("medialab\\hangman_DICTIONARΥ-" + ID + ".txt");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
@@ -91,7 +96,8 @@ public class HandleFiles {
     }
 
     public static void DeleteFile(String ID) {
-        File myObj = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab","hangman_DICTIONARΥ-" + ID +".txt");
+        //File myObj = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab","hangman_DICTIONARΥ-" + ID +".txt");
+        File myObj = new File("medialab\\hangman_DICTIONARΥ-" + ID + ".txt");
         if (myObj.delete()) {
             System.out.println("Deleted the file: " + myObj.getName());
         } else {
@@ -99,7 +105,8 @@ public class HandleFiles {
         }
     }
     public static boolean FileExists(String ID) {
-        File file = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab","hangman_DICTIONARΥ-" + ID +".txt");
+        //File file = new File("C:\\Users\\chriz\\Desktop\\multimedia_project2021-2022\\medialab","hangman_DICTIONARΥ-" + ID +".txt");
+        File file = new File("medialab\\hangman_DICTIONARΥ-" + ID + ".txt");
         return file.exists();
     }
 
